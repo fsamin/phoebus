dcf28f0c0b14624a157aa022b3dd4f1e899d5a17
François SAMIN <fsamin@MacBook-Pro-de-Francois.local>
2026-02-25 20:53:04 +0100

    Fix Monaco CSP: use local bundle instead of CDN

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
+import { loader } from '@monaco-editor/react'
+import * as monaco from 'monaco-editor'
 import App from './App.tsx'
 import './index.css'
+
+// Use local Monaco Editor instead of CDN (required by CSP)
+loader.config({ monaco })
 
 createRoot(document.getElementById('root')!).render(<App />)
